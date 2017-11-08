0adf6d5760dcd1c8ebd0d518400d02d3e6381554
Kyle Pollich <kpollich1@gmail.com>
2017-11-07 20:52:58 -0500

    Fix double image unfurl for media posts

--- index.js
+++ index.js
@@ -9,22 +9,8 @@ const URL = `${STREAMING_ENDPOINT}?access_token=${process.env.ACCESS_TOKEN}`
 const source = new EventSource(URL)
 
 source.addEventListener('update', event => {
-  // Rip out the data we care about from the event payload
   const data = JSON.parse(event.data)
-  const { uri, content } = data
-  const { avatar, username } = data.account
-
-  // Build the "status" URL so Slack will properly unfurl our link later
-  const statusId = uri.match(/statuses\/([0-9]+)/)[1]
-  const statusUrl = `${process.env.MASTODON_URL}/web/statuses/${statusId}`
-
-  // Look for an image (href property) so we can include it in the Slack payload
-  // and get an image preview to unfurl
-  const imageLinkMatch = content.match(/href="([^"]*)"/)
-
-  const text = !!imageLinkMatch
-    ? `New post from ${username} on Memestodon!\n<${statusUrl}>\n${imageLinkMatch[1]}`
-    : `New post from ${username} on Memestodon!\n<${statusUrl}>`
+  const text = createSlackMessage(data)
 
   const payload = {
     text,
@@ -42,4 +28,32 @@ source.onerror = error => {
 
 source.onclose = () => {
   console.log('Connection to Mastodon closed')
+}
+
+function createSlackMessage(data) {
+  const { uri, content } = data
+  const { username } = data.account
+
+  // Build the "status" URL so Slack will properly unfurl our link later
+  const statusId = uri.match(/statuses\/([0-9]+)/)[1]
+  const statusUrl = `${process.env.MASTODON_URL}/web/statuses/${statusId}`
+
+  const defaultText = `New post from ${username} on Memestodon!\n<${statusUrl}>`
+
+  // The presence of a `media_attachments` array denotes an uploaded or pasted image. Mastodon handles
+  // creating a link tag for these by default, and so they're included in the unfurled message in Slack,
+  // so we skip creating an unfurl-friendly link here if it's not required.
+  const hasMediaItem = data.media_attachments && data.media_attachments.length
+
+  // This is in inelegant way of looking for expandable links in the Mastodon payload. By tacking
+  // this link on to the end of the Slack message, Slack will unfurl the link. This lets us display
+  // images in Slack when a link is posted on Mastodon.
+  const imageLinkMatch = content.match(/href="([^"]*)"/)
+
+  // Mastodon posts with no uploaded/pasted items and a link get an unfurl-friendly link appended
+  if (!hasMediaItem && !!imageLinkMatch) {
+    return `New post from ${username} on Memestodon!\n<${statusUrl}>\n${imageLinkMatch[1]}`
+  }
+
+  return defaultText
 }
