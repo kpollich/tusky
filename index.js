e3e8e687e32ce2b2fce6dca010ebc2f46eb204ae
Andrew Elliot <andrwmorph@gmail.com>
2018-02-13 13:59:51 -0500

    Adding simple logging (#8)

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function createSlackMessage(data) {
   const statusUrl = data.url;
 
   const defaultText = `New post from ${username} on Mastodon!\n<${statusUrl}>`;
+  console.log(defaultText);
 
   // The presence of a `media_attachments` array denotes an uploaded or pasted image. Mastodon handles
   // creating a link tag for these by default, and so they're included in the unfurled message in Slack,
